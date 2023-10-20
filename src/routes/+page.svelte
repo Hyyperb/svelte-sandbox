486d3f4e070ce543228d413cbbd16bee586b3fdf
Hyyperb <bhavikmahalle28@gmail.com>
2023-10-20 13:51:24 +0000

    done base structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+	let version = "0.1";
+	$: todolist = [
+		"finish svelte tutorial",
+		"like the video",
+		"subscribe the channel"
+	]
+
+	function addTask(){
+		let todo_item_input_box = document.getElementById("add");
+		let task = todo_item_input_box.value;
+		todolist = [...todolist, task];
+		todo_item_input_box.value = null;
+		todo_item_input_box.focus();
+	}
+	function cleanUp(){
+		
+	}
+</script>
+
+<!-- <style>
+	p {
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	}	
+</style> -->
+
+<h1> Todo list</h1>
+<form>
+	<input type="text" id="add" on:change={addTask}/> 
+</form>
+<button on:click={addTask}>Add</button>
+<button on:click={cleanUp}>Clean up</button>
+<div>
+	{#each todolist as task ,i}
+
+		<ul><input type="checkbox" id="task {i}"/>{task}</ul>
+	{/each}
+</div>
